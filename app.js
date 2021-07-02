d0624e065ec61663c18b25203210a1c6ccd7d03e
Zubair shah <65024163+zubair894@users.noreply.github.com>
2021-07-02 20:24:59 +0500

    some working pn signup method

--- app.js
+++ app.js
@@ -2,12 +2,36 @@
 function onSignUp() {
     var email = document.getElementById("email")
     var password = document.getElementById("password")
+    var message = document.getElementById("login-success")
 
     var user = {
         email:email.value,
         password:password.value
     }
+    var users = JSON.parse(localStorage.getItem("users")) || [];
+
     localStorage.setItem(("user"),JSON.stringify(user))
 
-    console.log(user)
+    var userIdx = users.findIndex(function (val) {
+      return val.email.toLowerCase() === users.email.toLowerCase()
+    });
+
+    console.log(users)
+
+
+if (userIdx === -1) {
+    users.push(user)
+
+    localStorage.setItem("users", JSON.stringify(users))
+  location.href = "login.html"
+}
+else{
+ message.innerHTML = email + " Use in another account"
+}
+
+
+setTimeout(() => {
+  message.innerHTML = "";
+},2000);
+
 }

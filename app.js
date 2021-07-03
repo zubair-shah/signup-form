5e0929808e19b90ec908003697a3dedf16e4d1bf
Zubair shah <65024163+zubair894@users.noreply.github.com>
2021-07-03 19:06:25 +0500

    some working

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+
+
+
+
+// =============if input is empty ==========
+// function empty() {
+  
+//   if (email.value == "" || password.value == "") {
+//       alert("Enter a Valid email and  password");
+//       return false;
+  
+//   }
+//   else{
+//     return true;
+//   }
+// }
+
+
+
+
 // ============function-onsignup==============
 function onSignUp() {
     var email = document.getElementById("email")
@@ -10,28 +30,101 @@ function onSignUp() {
     }
     var users = JSON.parse(localStorage.getItem("users")) || [];
 
-    localStorage.setItem(("user"),JSON.stringify(user))
 
     var userIdx = users.findIndex(function (val) {
-      return val.email.toLowerCase() === users.email.toLowerCase()
+      return val.email.toLowerCase() === user.email.toLowerCase()
     });
 
-    console.log(users)
+    console.log(userIdx)
 
 
+
+   
 if (userIdx === -1) {
+    message.innerHTML = " signup successful"
     users.push(user)
-
+    setTimeout(() => {
+      message.innerHTML = "";
+      location.href = "login.html"
+    },4000);
     localStorage.setItem("users", JSON.stringify(users))
-  location.href = "login.html"
+   
+  
 }
 else{
- message.innerHTML = email + " Use in another account"
+ message.innerHTML = user.email + " Use in another account"
+ 
+}
+
 }
 
 
-setTimeout(() => {
+//===============function-onlogin==============
+
+function onLogin() {
+  var email = document.getElementById("email")
+  var password = document.getElementById("password")
+  var message = document.getElementById("message")
+  
+
+var user = {
+  email:email.value,
+  password:password.value
+}
+
+var users = JSON.parse(localStorage.getItem("users"))
+
+var currentUser = users.find(function (val) {
+  return val.email.toLowerCase() === user.email.toLowerCase() && val.password.toLowerCase() === user.password.toLowerCase() 
+})
+if (currentUser) {
+  localStorage.setItem("user", JSON.stringify(currentUser))
+ message.innerHTML = "successfully Login"
+ 
+}
+else{
+  message.innerHTML = "Invalid cridential"
+}
+
+setTimeout(()=>{
   message.innerHTML = "";
-},2000);
+  location.href = "index.html"
+},
+2000
+  );
+
 
 }
+function getCurrentUser() {
+  var detail = document.getElementById("detail");
+  var user = JSON.parse(localStorage.getItem("user"));
+  detail.innerHTML = "Loggedin as " + user.email.split("@")[0];
+}
+
+function onLogout() {
+  var message = document.getElementById("message");
+  localStorage.removeItem("user");
+  message.innerHTML = "Good Bye.!";
+  // clear state
+  setTimeout(() => {
+    message.innerHTML = "";
+    location.href = "login.html";
+  }, 3000);
+
+}
+
+
+
+
+
+// $(document).ready(function () {
+//   $("#sidebar").mCustomScrollbar({
+//       theme: "minimal"
+//   });
+
+//   $('#sidebarCollapse').on('click', function () {
+//       $('#sidebar, #content').toggleClass('active');
+//       $('.collapse.in').toggleClass('in');
+//       $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+//   });
+// });
